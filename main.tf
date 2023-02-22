6dd9e48c7e265b9e332c79a26390e5bd83edd096
S-Divyadharshini <sdivya-cont@mahindra.com>
2023-02-22 22:52:34 +0530

    new variables

--- main.tf
+++ main.tf
@@ -2,12 +2,14 @@ resource "aws_elb" "bar" {
   name               = var.elb_name
   availability_zones = var.availability_zones
   security_groups    = [aws_security_group.ssh_from_office.id]
-
+  cross_zone_load_balancing = var.cross_zone_load_balancing
+  desync_mitigation_mode = var.desync_mitigation_mode
   listener {
     instance_port     = var.instance_port
     instance_protocol = var.instance_protocol
     lb_port           = var.lb_port
     lb_protocol       = var.lb_protocol
+    ssl_certificate_id = var.ssl_certificate_id
   }
 
   health_check {
@@ -18,7 +20,13 @@ resource "aws_elb" "bar" {
     interval            = 30
   }
   instances = var.instance_id
-
+  connection_draining = var.connection_draining
+  connection_draining_timeout = var.connection_draining_timeout
+  access_logs {
+    bucket = var.access_logs_bucket
+    enabled = var.access_logs_enabled
+    bucket_prefix = var.access_logs_bucket_prefix
+  }
 }
 
 resource "aws_security_group" "ssh_from_office" {
@@ -28,7 +36,7 @@ resource "aws_security_group" "ssh_from_office" {
     protocol  = -1
     self      = true
     from_port = 0
-    to_port   = 0
+    to_port   = 80
   }
 
   egress {
--- variables.tf
+++ variables.tf
@@ -39,3 +39,42 @@ variable "instance_id" {
   type =set(string)
   description = "describe your variable"
 }
+variable "ssl_certificate_id" {
+  description = "The ARN of an SSL certificate you have uploaded to AWS IAM. Note ECDSA-specific restrictions below. Only valid when lb_protocol is either HTTPS or SSL"
+  type = string
+}
+ variable "cross_zone_load_balancing" {
+  type = bool
+  default = true
+  description = "Enable cross-zone load balancing"
+   
+ }
+ variable "desync_mitigation_mode" {
+  description = "Determines how the load balancer handles requests that might pose a security risk to an application due to HTTP desync. Valid values are monitor, defensive (default), strictest."
+  type = string
+  default = "defensive"
+ }
+ variable "connection_draining" {
+   description = "Boolean to enable connection draining"
+   type = bool
+   default = true
+ }
+ variable "connection_draining_timeout" {
+  description = "valThe time in seconds to allow for connections to drainue"
+  type = number
+  default = 400
+ }
+ variable "access_logs_enabled" {
+  type = bool
+  default = true
+  description = "Boolean to enable / disable access_logs"
+ }
+ variable "access_logs_bucket" {
+  type = string
+  description = "The S3 bucket name to store the logs in."
+ }
+ variable "access_logs_bucket_prefix" {
+  type = string
+  description = "The S3 bucket prefix. Logs are stored in the root if not configured."
+   
+ }
